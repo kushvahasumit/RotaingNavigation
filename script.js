8b8202e2da3d65c04e61019b50e5d695125f2818
sumit kushwaha <121751224+kushvahasumit@users.noreply.github.com>
2023-03-13 20:36:56 +0530

    enhancement
    Signed-off-by: sumit kushwaha <121751224+kushvahasumit@users.noreply.github.com>

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ const container = document.querySelector('.container')
 open.addEventListener('click',()=>container.classList.add('show-nav'))
 
 close.addEventListener('click',()=>container.classList.remove('show-nav'))
+
+
